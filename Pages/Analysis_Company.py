4f4e638e2d06f5667aefcd9e7c0b4cae8c019e7d
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-02-20 23:53:16 -0300

    Incremento de novas funções no Analysis company

--- Pages/Analysis_Company.py
+++ Pages/Analysis_Company.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import datetime
+import datetime as dt
 
 # Definindo o título e o ícone do aplicativo
 st.set_page_config(
@@ -13,17 +13,8 @@ st.set_page_config(
 # Função para obter dados históricos de um ticker
 @st.cache_data
 def get_ticker_data(ticker):
-    """
-    Baixa dados históricos de um ticker usando o objeto Ticker do yfinance.
-
-    Args:
-        ticker (str): O símbolo do ticker para obter dados.
-
-    Returns:
-        pd.DataFrame: Um DataFrame contendo os dados históricos do ticker.
-    """
     ticker_obj = yf.Ticker(ticker)  # Criando o objeto Ticker
-    stock_data = ticker_obj.history(period="10y", interval="1d")  # Obtendo dados históricos
+    stock_data = ticker_obj.history(period="max", interval="1d")  # Obtendo dados históricos
     return stock_data[['Close']]  # Retorna apenas a coluna de fechamento
 
 # Exibindo o título da página
@@ -32,72 +23,122 @@ def get_ticker_data(ticker):
 
 Explore o desempenho do ticker no mercado financeiro.
 '''
-# Definindo o ticker para análise
-ticker = st.text_input('Digite o Ticker:', 'AAPL')  # Ticker default é AAPL (Apple)
 
-# Rodando a função para pegar o histórico do ticker
+    
+# Definindo o ticker para análise
+def is_valid_ticker(ticker):
+    """Verifica se o ticker existe no Yahoo Finance."""
+    if not ticker or ticker is None or ticker == {}:
+        return False
+    try:
+        stock = yf.Ticker(ticker)
+        info = stock.info
+        return 'longName' in info  # Se tiver nome da empresa, é válido
+    except Exception:
+        return False
+
+# Entrada do usuário
+ticker = st.text_input('Digite o Ticker:', 'AAPL')
+
+# Validação do ticker
+if ticker:
+    if is_valid_ticker(ticker):
+        ticker = ticker
+    else:
+        st.error(f"O ticker '{ticker}' não é válido. Tente outro.")
+else:
+    st.warning("Por favor, digite um ticker antes de continuar.")
+
+# Rodando a função para pegar os dados do ticker
+tickerChosen = yf.Ticker(ticker)
 ticker_data = get_ticker_data(ticker)
 
 # Adicionando espaço
 ''
+
+# Colunas de informações estáticas sobre a ação
+col1, col2, col3 = st.columns([1,1,1])
+with col1:
+    st.write(f"Empresa: {tickerChosen.info['longName']}")
+with col2:
+    st.write(f"Mercado: {tickerChosen.info['industry']}")
+with col3:
+    st.write(f"Preço atual: {tickerChosen.info['currentPrice']}USD")
+
+# Informações sobre a empresa
+summary =  tickerChosen.info['longBusinessSummary']
+st.markdown(
+    f"""
+    <div style="overflow-y: scroll; height: 200px; background-color: rgba(50, 50, 50, 0.9); border-radius: 5px; padding: 10px;">
+        <pre>{summary}</pre>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+''
 ''
 
-# Convertendo pandas Timestamps para objetos datetime do Python
-min_date = ticker_data.index.min().to_pydatetime()
-max_date = ticker_data.index.max().to_pydatetime()
 
-# Permitindo que o usuário selecione um intervalo de tempo
-from_date, to_date = st.slider(
-    'Em que período de tempo você está interessado?',
-    min_value=min_date,
-    max_value=max_date,
-    value=[min_date, max_date],
-    format="YYYY-MM-DD"
-)
-
-# Filtrando os dados de acordo com a seleção de datas
-filtered_ticker_data = ticker_data[
-    (ticker_data.index <= to_date)
-    & (from_date <= ticker_data.index)
-]
-
-# Plotando os dados
-st.line_chart(
-    filtered_ticker_data,
-)
 
 # Exibindo métricas do ticker
 st.header(f'Métricas do Ticker {ticker}', divider='gray')
 
-today = datetime.date.today()
-target_date = st.date_input('Digite a data:', today)  # Ticker default é AAPL (Apple)
+tabs = st.tabs(["Evolução de preços", "Dividendos e Descobramentos", "Informações Gerais"])
 
-# Função para encontrar a data mais próxima de uma data alvo
-def get_closest_date(data, target_date):
-    available_dates = data.index
-    closest_date = available_dates.loc[(available_dates - target_date).abs().idxmin()]
-    return closest_date
+# Tab 1
+with tabs[0]:
+    # Convertendo pandas Timestamps para objetos datetime do Python
+    min_date = ticker_data.index.min().to_pydatetime()
+    max_date = ticker_data.index.max().to_pydatetime()
+    # Permitindo que o usuário selecione um intervalo de tempo
+    st.subheader('Gráfico de evolução')
+    from_date, to_date = st.slider(
+    'Evolução da ação ao longo dos anos',
+    min_value=min_date,
+    max_value=max_date,
+    value=[min_date, max_date],
+    format="YYYY-MM-DD"
+    )
+    # Filtrando os dados de acordo com a seleção de datas
+    filtered_ticker_data = ticker_data[
+        (ticker_data.index <= to_date)
+        & (from_date <= ticker_data.index)
+    ]
+    # Evolução de preços
+    st.line_chart(
+        filtered_ticker_data,
+    )
+    # Outra visualização
+    st.subheader('Analise de evolução escolhendo o incio')
+    startDat = st.text_input('Data de inicio', '2025-01-01')
+    endDat = dt.date.today()
+    newTime = tickerChosen.history(start = startDat, end = endDat)
+    st.dataframe(newTime)
+    st.download_button('Download CSV', newTime.to_csv(), file_name='evolução.csv')
 
-# Encontrando os valores no intervalo selecionado
-closest_start_date = get_closest_date(filtered_ticker_data, from_date)
-closest_end_date = get_closest_date(filtered_ticker_data, to_date)
 
-# Pegando os valores de fechamento no intervalo
-start_value = filtered_ticker_data.loc[closest_start_date]['Close']
-end_value = filtered_ticker_data.loc[closest_end_date]['Close']
+# Tab 2
+with tabs[1]:
+    # Dividendos Pagos anualmente
+    col1, col2= st.columns([1,1])
+    with col1:
+        st.write(f"Dividend Yield: {tickerChosen.info['dividendYield']}")
+    with col2:
+        st.write(f"Dividend Rate: {tickerChosen.info['dividendRate']}")
+    st.subheader('Dividendos Pagos anualmente')
+    dividendsYearly = tickerChosen.dividends.resample('Y').sum()
+    st.bar_chart(dividendsYearly)
+    st.subheader('Desddobramento das ações')
+    st.dataframe(tickerChosen.splits)
 
-# Calculando o crescimento
-if pd.isna(start_value) or pd.isna(end_value):
-    growth = 'n/a'
-    delta_color = 'off'
-else:
-    growth = f'{end_value / start_value:,.2f}x'
-    delta_color = 'normal'
+# Tab 3
+with tabs[2]:
+    # Exbir informações contábeis
+    st.subheader('Informações Contábeis')
+    st.dataframe(tickerChosen.financials )
+    st.download_button('Download CSV Contábil', tickerChosen.financials.to_csv(), file_name='financeiro.csv')
+    st.subheader('Informações de Caixa')
+    st.dataframe(tickerChosen.cashflow)
+    st.download_button('Download CSV Caixa', tickerChosen.cash_flow.to_csv(), file_name='caixa.csv')
 
-# Exibindo a métrica
-st.metric(
-    label=f'{ticker} Index',
-    value=f'{end_value:,.0f}',
-    delta=growth,
-    delta_color=delta_color
-)
+    # print(tickerChosen.info) - visualizar as infos
